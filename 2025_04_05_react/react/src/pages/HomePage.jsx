999dc2aeb861efb4ebf4744a19561b869c0840a3
Angel <angel.ch1595@gmail.com>
2026-05-14 19:46:11 -0500

    agregandoUnLinkNuevo

--- 2025_04_05_react/react/src/pages/HomePage.jsx
+++ 2025_04_05_react/react/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
-import {Link} from "react-router-dom"
-const HomePage=()=>{
-    return(
-<div>
-    <h1>Bienvenido</h1>
-    <Link to="/estudiantes">Lista de estudiantes</Link>
-</div>
-    )
-}
-export default HomePage
+import { Link } from "react-router-dom";
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bienvenido</h1>
+      <div>
+        <Link to="/estudiantes">Lista de estudiantes</Link>
+      </div>
+      <div>
+        {" "}
+        <Link to="/nuevo">Agregar Estudiante</Link>
+      </div>
+    </div>
+  );
+};
+export default HomePage;
